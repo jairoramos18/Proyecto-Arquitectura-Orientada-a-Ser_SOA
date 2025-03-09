9322d79e89867be02fd9ea7cd71fb6e6e56e2ae0
jairoramos18 <jaramosm@ufpso.edu.co>
2025-03-08 21:32:23 -0500

    pasando a tailwind css el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'caña panelera';
+  title = 'Login_jairo';
 }
